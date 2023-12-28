fba499498782c1c4331b37f47bd1d142c1fb8372
arijeet <arijeetc6@gmail.com>
2023-12-29 00:10:22 +0530

    rough commit

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -2,6 +2,6 @@ import 'package:flutter/material.dart';
 
 const Color kPrimaryColor = Color.fromARGB(255, 52, 27, 75);
 const Color kTextColor = Color.fromARGB(255, 33, 32, 36);
-const Color kBackgroundColor = Color.fromARGB(255, 223, 211, 253);
+const Color kBackgroundColor = Color.fromARGB(255, 255, 255, 255);
 const double kDefaultPadding = 20.0;
 const kUserName = 'Arijeet';
