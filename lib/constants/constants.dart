7a9879afa5c601aa427a6935cda0425e333a05e4
arijeet <arijeetc6@gmail.com>
2023-12-28 22:21:19 +0530

    defined app color constants.

---
+++ lib/constants/constants.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const Color kPrimaryColor = Color(0xFF0C9869);
+const Color kTextColor = Color(0xFF3C4046);
+const Color kBackgroundColor = Color(0xFFF9F8FD);
+const double kDefaultPadding = 20.0;
